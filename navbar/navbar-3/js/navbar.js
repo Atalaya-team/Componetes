ecf39c5224f14a0a0da674e1a3318dd05721178a
ISMAEL GARCIA <fging2019@gmail.com>
2020-04-10 22:27:57 -0430

    carga Navbar

---
+++ navbar/navbar-3/js/navbar.js
@@ -0,0 +1,10 @@
+
+ $(document).ready(function () {
+    $("#btn").on("click",function(){
+        $(".menu").toggleClass("show");
+    });  
+    
+    $("#btn").on("click",function(){
+        $(".navbar").toggleClass("show2");
+    }); 
+ });
